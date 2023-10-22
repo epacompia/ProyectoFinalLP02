f3f98a3eab7b59581dff6689feb4fa78068553ba
Luis Ore Cardenas <luisore24@gmail.com>
2023-10-22 14:43:27 -0500

    create, update of category model

--- src/main/java/com/proyectofinal/controller/CategoryController.java
+++ src/main/java/com/proyectofinal/controller/CategoryController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.proyectofinal.model.Category;
 import com.proyectofinal.service.ICategoryService;
@@ -22,8 +25,48 @@ public class CategoryController {
 	public String categories(Model model) {
 		List<Category> categories = categoryService.getAll();
 		model.addAttribute("categories", categories);
-		System.out.println(categories);
 		return "category";
+	}
+	
+	
+	@GetMapping("/categories/new")
+	public String saveCategoryForm(Model model) {
+		Category category = new Category();
+		model.addAttribute("category", category);
+		return "category-form";
+	}
+	
+	@PostMapping("/categories/new")
+	public String saveCategory(@ModelAttribute("category") Category category, Model model) {
+		Category newCategory = new Category();
+		newCategory.setCategory_id(category.getCategory_id());
+		newCategory.setName_category(category.getName_category());
+		newCategory.setFlag(true);
+		categoryService.register(newCategory);
+		return "redirect:/categories";
+	}
+	
+	@GetMapping("/categories/edit/{id}")
+	public String updateCategoryForm(@PathVariable Integer id,Model model) {
+		
+		Category category = categoryService.getOne(id);
+		if(category == null) {
+			return "redirect:/categories";
+		}
+		
+		model.addAttribute("category", category);
+		
+		return "category-form";
+	}
+	
+	@PostMapping("/categories/edit/{id}")
+	public String updateCategory(@PathVariable Integer id, @ModelAttribute("category") Category category, Model model) {
+		Category newCategory = categoryService.getOne(id);
+		newCategory.setCategory_id(category.getCategory_id());
+		newCategory.setName_category(category.getName_category());
+		newCategory.setFlag(category.isFlag());
+		categoryService.update(newCategory);
+		return "redirect:/categories";
 	}
 	
 	
