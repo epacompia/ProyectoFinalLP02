1d4dafa20ef02ca8f1fae4e353bf1993407e1a96
eduardopacompialopez <eduardopacompialopez@gmail.com>
2023-11-01 23:13:30 -0500

    complete for Entity Rol , list,create,update

--- src/main/java/com/proyectofinal/controller/RolController.java
+++ src/main/java/com/proyectofinal/controller/RolController.java
@@ -3,6 +3,8 @@ package com.proyectofinal.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.proyectofinal.model.Rol;
 import com.proyectofinal.repository.IRolRepository;
+
+
 
 @Controller
 public class RolController {
@@ -24,28 +28,55 @@ public class RolController {
 		return "rol";
 	}
 	
+	//NUEVO
+	@GetMapping("/newrol")
+	public String createRol(Model model) {
+		Rol r=new Rol();
+		model.addAttribute("rol",r);
+		return "newrol";
+	}
+	
+	@PostMapping("savenewrol")
+	public String createRol(@ModelAttribute Rol rol, Model model) {
+		 if (rol.getName_rol() == null || rol.getName_rol().isEmpty()) {
+		        model.addAttribute("mensaje1", "El campo 'nombre' es obligatorio");
+		        model.addAttribute("cssmensaje1", "alert alert-danger");
+		        return "newrol";  // Reemplaza 'tu_vista' por el nombre de tu vista
+		    }
+		try {
+			repoRol.save(rol);
+			 model.addAttribute("mensaje", "Registro creado con éxito");
+		     model.addAttribute("cssmensaje", "alert alert-primary");
+		} catch (Exception e) {
+			// TODO: handle exception
+			 model.addAttribute("mensaje", "Error al crear el registro");
+		     model.addAttribute("cssmensaje", "alert alert-danger");
+		}
+		return "redirect:/rol";
+	}	
+	
+	
 	//EDIT
 	@GetMapping("/edit/{rol_id}")
 	public String edit(@PathVariable Integer rol_id, Model model) {
 		Rol r=repoRol.findById(rol_id).get();
 		model.addAttribute("rol",r);
+		//System.out.println(rol);	
 		return "editrol";
 	}
 	
 	
 	@PostMapping("/saverol")
 	public String saverol(@ModelAttribute Rol rol, Model model) {
-		try {
-			repoRol.save(rol);
-			model.addAttribute("mensaje","Registro ok");
-			model.addAttribute("cssmensaje","alert alert-primary");
-		} catch (Exception e) {
-			// TODO: handle exception
-			model.addAttribute("mensaje","Error al registrar");
-			model.addAttribute("cssmensaje","alert alert-danger");
-		}
 		
-		return "rol";
+		  try { repoRol.save(rol); model.addAttribute("mensaje","Registro ok");
+		  model.addAttribute("cssmensaje","alert alert-primary"); } catch (Exception e)
+		  { // TODO: handle exception
+		  model.addAttribute("mensaje","Error al registrar");
+		  model.addAttribute("cssmensaje","alert alert-danger");
+		  }	 
+			
+		  return "redirect:/rol";
 	}
 	
 	
