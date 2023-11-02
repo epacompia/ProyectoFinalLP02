994234e8bbff327035151d926d05efd0848d9afa
eduardopacompialopez <eduardopacompialopez@gmail.com>
2023-11-01 20:50:42 -0500

    created list and edit for Rol Model

--- src/main/java/com/proyectofinal/controller/RolController.java
+++ src/main/java/com/proyectofinal/controller/RolController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.proyectofinal.model.Rol;
 import com.proyectofinal.repository.IRolRepository;
 
 @Controller
@@ -13,10 +17,36 @@ public class RolController {
 	@Autowired
 	private IRolRepository repoRol;
 	
-	//Cargar listado de roles
+	//LISTADO
 	@GetMapping("/rol")
 	public String index(Model model) {
 		model.addAttribute("lstRoles",repoRol.findAll());
 		return "rol";
 	}
+	
+	//EDIT
+	@GetMapping("/edit/{rol_id}")
+	public String edit(@PathVariable Integer rol_id, Model model) {
+		Rol r=repoRol.findById(rol_id).get();
+		model.addAttribute("rol",r);
+		return "editrol";
+	}
+	
+	
+	@PostMapping("/saverol")
+	public String saverol(@ModelAttribute Rol rol, Model model) {
+		try {
+			repoRol.save(rol);
+			model.addAttribute("mensaje","Registro ok");
+			model.addAttribute("cssmensaje","alert alert-primary");
+		} catch (Exception e) {
+			// TODO: handle exception
+			model.addAttribute("mensaje","Error al registrar");
+			model.addAttribute("cssmensaje","alert alert-danger");
+		}
+		
+		return "rol";
+	}
+	
+	
 }
